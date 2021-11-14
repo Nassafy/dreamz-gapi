968d74ee12173931aafbabd975e8972eb1cb06a7
Matthias <matthiaspinard@gmail.com>
2021-11-14 14:48:55 +0100

    add cors

--- api/server.go
+++ api/server.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"dreamz.com/api/db"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,7 +19,7 @@ func NewServer() *Server {
 
 	store := db.NewStore()
 
-	router.Use(CORSMiddleware())
+	router.Use(cors.Default())
 
 	router.POST("auth/login", server.Login)
 
@@ -45,23 +46,6 @@ func (server *Server) Start(adress string) error {
 
 func (server *Server) CloseStore() {
 	server.store.CloseStore()
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
 
 func GetUserId(c *gin.Context) (string, error) {
